2d0210d7f9aa4560ce4edea6184d52c611837bdf
Angelo <angelo.palmieri@rufusleonard.com>
2021-06-21 15:59:00 +0100

    Add an API to Azure Static Web Apps with Azure Functions

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,38 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
+import React, { useState, useEffect } from "react";
+import SmallCard from "../components/SmallCard";
+import { projectIcons } from "../components/Icons";
 
-import { projects } from '../utils/projectsData';
+import { projects } from "../utils/projectsData";
 
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
-      })}
+const Home = () => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return (
+    <div className="home">
+      <h1>What Can I Deploy to Static Apps?</h1>
+      <p>{data}</p>
+      <div className="card-grid">
+        {projects.map((project) => {
+          const Icon = projectIcons[project.id];
+          return (
+            <SmallCard
+              key={project.id}
+              Icon={Icon}
+              title={project.name}
+              slug={project.slug}
+            />
+          );
+        })}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
